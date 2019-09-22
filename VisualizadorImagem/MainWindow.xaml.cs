679a9feeaedbd4aa5f95febc3a3270c25bc897ff
Jociel Barbosa <43018079+jocieltb@users.noreply.github.com>
2019-09-22 03:14:17 -0300

    Visualizador de Imagem
    Recebendo Lista de endereços de imagens em um ComboBox

---
+++ VisualizadorImagem/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VisualizadorImagem
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    /// 
+    
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            //dlg.InitialDirectory = "c:\\";
+            dlg.Filter = "Image files (*.jpg)|*.jpg|Image files (*.png)|*.png|All Files (*.*)|*.*";
+            dlg.RestoreDirectory = true;
+            dlg.Multiselect = true;
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                
+                ComboBoxArquivo.ItemsSource = dlg.FileNames;
+                ComboBoxArquivo.SelectedIndex = 0;
+
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(ComboBoxArquivo.SelectedItem as string);
+                bitmap.EndInit();
+                ImageView.Source = bitmap;
+            }
+
+        }
+
+        private void BtnCarregar_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.UriSource = new Uri(ComboBoxArquivo.SelectedItem as string);
+            bitmap.EndInit();
+            ImageView.Source = bitmap;
+
+        }
+    }
+}
